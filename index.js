b8f44c82fa00d765f64dd073ac984906a7ac5c17
Mike Moore <tigerhollowpoint@gmail.com>
2019-02-08 21:32:53 -0500

    see if the tweets really work

--- index.js
+++ index.js
@@ -18,6 +18,34 @@ var T = new Twit({
 var stream = T.stream('statuses/filter',  { follow: "1093963120412971009" })
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet.text);
+  //CHECK based on object
+  if(tweet.in_reply_to_status_id) {
+    return;
+  }
+  if(tweet.retweeted_status) {
+    return;
+  }
+  var text = (tweet.truncated) ? tweet.extended_tweet.full_text : tweet.text;
+  var t_exploded = text.split(" ");
+  //MANUAL RETWEET
+  if(t_exploded[0] === "RT") {
+    return;
+  }
+  if(t_exploded[0].indexOf('"@') > -1) {
+    return;
+  }
+  var exclaimed = false;
+  var tweetString = "";
+  t_exploded.forEach(function(e,i){
+    if(e.indexOf("!") > -1 && e.indexOf('http') < 0) {
+      exclaimed = true;
+    }
+    tweetString += e.replace(/!/g, '?');
+  });
+  tweetString = tweetString.replace(/&amp;/g,"&");
+  if(!exclaimed) {
+    return;
+  }
+  https.get('https://www.becreativeeveryday.com/unsuretrump/node_test.php?t='+encodeURIComponent(tweetString));
   //https.get('https://www.becreativeeveryday.com/unsuretrump/node_test.php');
 });
