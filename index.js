2c8f60eb08906ae60c1bc89de82a865099d4f000
Mike Moore <tigerhollowpoint@gmail.com>
2019-02-08 19:42:28 -0500

    mango test

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const https = require('https');
+var Twit = require('twit');
 
 setTimeout(function(){
   https.get('https://www.becreativeeveryday.com/unsuretrump/node_test.php');
+  console.log('pinged');
 }, 1000 * 60 * 2);
+
+
+var T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
+});
+
+
+var stream = T.stream('statuses/filter', { track: 'mango' })
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+});
